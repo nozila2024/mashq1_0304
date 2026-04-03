66984409c7da69571f27c974d770f8cb07384bdb
Nozila <nozilarajabboyeva@gmail.com>
2026-04-03 15:03:48 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#1-misol
+x = lambda son1, son2: max(son1, son2)
+print(x(5, 8))
+
+#2-misol
+x = lambda a: abs(a)
+print(x(-7))
+
+#3-misol
+x = lambda a, b, c: (a+b+c)
+print(x(3, 5, 7))
+
+#4-misol
+x = lambda son1, son2: son1 % son2
+print(x(17, 5))
+
+#5-misol
+x = lambda son: son * 9/5 + 32
+print(x(100))
+
+#6-misol
+x = lambda son1, son2: min(son1, son2)
+print(x(5, 8))
+
+#7-misol
+x = lambda son: "juft" if son % 2 == 0 else "toq"
+print(x(8))
